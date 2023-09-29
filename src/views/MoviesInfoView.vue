4220a3657813f39920a68cf78192d902ea3267de
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2023-09-29 11:38:44 +0200

    feat: information pages

--- src/views/MoviesInfoView.vue
+++ src/views/MoviesInfoView.vue
@@ -1,9 +1,40 @@
 <script setup>
+import {onMounted, ref} from 'vue'
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
+const id = route.params.id
+
+const data = ref()
+
+onMounted(async () => {
+  const response = await fetch('http://localhost:8088/wra506/index.php/api/movies/' + id)
+  data.value = await response.json()
+})
 </script>
 
 <template>
+  <main v-if="data">
+    <h2>{{data.title}}</h2>
+    <p><span>Synopsis :</span> {{data.description}}</p>
+    <p><span>Date de sortie :</span> {{data.releaseDate}}</p>
+    <p><span>Durée :</span> {{data.duration}}m</p>
 
+    <p><span>Genre :</span> {{data.category.name}}</p>
+
+    <div class="actors">
+      <h4>Acteurs</h4>
+      <ul v-for="actor in data.actor">
+        <li>
+          <router-link  :to="{name: 'actors-info', params: {id: actor.id}}">{{actor.firstName}} {{actor.lastName}}</router-link>
+        </li>
+      </ul>
+    </div>
+
+
+
+
+  </main>
 </template>
 
 <style scoped>

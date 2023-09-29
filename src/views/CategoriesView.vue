251564393fd0965fca41d6b93cddbf8d86abcc39
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2023-09-29 11:00:45 +0200

    feat: Categories Page (list of movies by categories)

--- src/views/CategoriesView.vue
+++ src/views/CategoriesView.vue
@@ -1,9 +1,24 @@
 <script setup>
+import {onMounted, ref} from 'vue'
+import CategoryCard from "../components/CategoryCard.vue";
 
+const data = ref()
+
+onMounted(async () => {
+  const response = await fetch('http://localhost:8088/wra506/index.php/api/categories')
+  data.value = await response.json()
+  console.log(data)
+})
 </script>
 
 <template>
+  <main>
+    <h2>Films par catégories</h2>
 
+    <div class="category-box">
+      <CategoryCard v-if="data" v-for="category in data['hydra:member']" :category="category" />
+    </div>
+  </main>
 </template>
 
 <style scoped>

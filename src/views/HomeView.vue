1357f771904ecfb30276195c73d7c5da5b946983
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2023-09-29 10:31:52 +0200

    feat: HomePage - section actors with 4 random actors from api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,24 @@
 <script setup>
 import MovieCard from "../components/MovieCard.vue"
+import ActorCard from "../components/ActorCard.vue"
+
 import {onMounted, ref} from "vue";
 
-const data = ref()
+const data_movies = ref()
+const data_actors = ref()
 
 onMounted(async () => {
-  const response = await fetch('http://localhost:8088/wra506/index.php/api/movies')
-  data.value = await response.json().then((data) => {
-    return data['hydra:member'].sort(() => Math.random() - Math.random()).slice(0, 4)
+  const response_movies = await fetch('http://localhost:8088/wra506/index.php/api/movies')
+  const response_actors = await fetch('http://localhost:8088/wra506/index.php/api/actors')
+
+  //MOVIES
+  data_movies.value = await response_movies.json().then((data_movies) => {
+    return data_movies['hydra:member'].sort(() => Math.random() - Math.random()).slice(0, 4)
   })
-  console.log(data)
+  //ACTORS
+  data_actors.value = await response_actors.json().then((data_actors) => {
+    return data_actors['hydra:member'].sort(() => Math.random() - Math.random()).slice(0, 4)
+  })
 })
 </script>
 
@@ -17,7 +26,12 @@ onMounted(async () => {
   <main>
     <h2>Découvrez nos films</h2>
     <div class="carrousel-movies">
-      <MovieCard v-if="data" v-for="movie in data" :movie="movie" />
+      <MovieCard v-if="data_movies" v-for="movie in data_movies" :movie="movie" />
+    </div>
+
+    <h2>Les acteurs en vedettes</h2>
+    <div class="carrousel-actors">
+      <ActorCard v-if="data_actors" v-for="actor in data_actors" :actor="actor" />
     </div>
   </main>
 </template>
@@ -31,7 +45,7 @@ main{
   padding: 10px;
 }
 
-.carrousel-movies{
+.carrousel-movies, .carrousel-actors{
   padding: 10px;
   display: flex;
   gap: 20px;

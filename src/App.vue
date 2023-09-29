d09e7e62e8d9d6e968a730342bcc70aa7c0ccc25
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2023-09-29 08:48:06 +0200

    feat: navbar

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,10 @@
 <template>
   <header>
     <nav>
-
+      <router-link to="/">Accueil</router-link>
+      <router-link to="/movies">Films</router-link>
+      <router-link to="/actors">Acteurs</router-link>
+      <router-link to="/categories">Categories</router-link>
     </nav>
   </header>
 </template>
